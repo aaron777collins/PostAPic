32a485614197dd81b9420b493dd4d2ade6fb22df
aaron777collins <aaron777collins@gmail.com>
2023-03-12 00:16:41 -0500

    created basic frontend and backend

---
+++ backend/createtables.php
@@ -0,0 +1,38 @@
+<?php
+
+    // connect to the mysql database and create the users table
+    // if it doesn't already exist
+
+    $servername = "appdb:3306";
+
+    // REPLACE with your Database name
+    $dbname = "pap";
+
+    $dbuser = "colli11s";
+    $dbpass = "secret";
+
+    // Create connection
+    $conn = new mysqli($servername, $dbuser, $dbpass, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // sql to create table
+    $sql = "CREATE TABLE IF NOT EXISTS users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+
+    // executing sql query
+    $result = mysqli_query($conn, $sql);
+
+    // sends result back in JSON format
+    echo json_encode(array("result" => $result));
+?>
---
+++ frontend/src/App.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function getFromBackend() {
+  fetch("http://localhost:8080/test.php")
+    .then(function (response) {
+      // The API call was successful!
+      return response.json();
+    })
+    .then(function (data) {
+      // This is the JSON from our response
+      console.log(data);
+    })
+    .catch(function (err) {
+      // There was an error
+      console.warn("Something went wrong.", err);
+    });
+}
+
+function createTables() {
+  fetch("http://localhost:8080/createtables.php")
+    .then(function (response) {
+      // The API call was successful!
+      return response.json();
+    })
+    .then(function (data) {
+      // This is the JSON from our response
+      console.log(data);
+    })
+    .catch(function (err) {
+      // There was an error
+      console.warn("Something went wrong.", err);
+    });
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React 8
+        </a>
+        <button onClick={getFromBackend}>Get from backend</button>
+        <button onClick={createTables}>Create tables</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
